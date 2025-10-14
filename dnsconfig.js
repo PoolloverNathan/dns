e13edf661d58c985e690e4b31f0976201fd0fce0
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-10-13 21:52:21 -0400

    sync

--- dnsconfig.js
+++ dnsconfig.js
@@ -17,6 +17,10 @@ MY_IP$.then(function(ip) {
 		TXT("_dnslink.maven", "\"dnslink=/ipns/k51qzi5uqu5dm13gjm40nv9ii9kifawae4f3upf23soytez54i4588v6kb1uw3\""),
 		//A("maven", "209.94.90.3", CF_PROXY_ON),
 		//A("maven", "209.94.90.2", CF_PROXY_ON),
+
+        // Mavensburg
+        A("q", "50.92.163.215"),
+		CNAME("mavensburg", "q.pool.net.eu.org."),
 		
 		// My stuff
 		CNAME("cache", "h.pool.net.eu.org.", CF_PROXY_ON),
@@ -27,13 +31,21 @@ MY_IP$.then(function(ip) {
 		CNAME("phone", "h.pool.net.eu.org.", CF_PROXY_ON),
 		CNAME("vsc", "h.pool.net.eu.org.", TTL(60)),
 
+        SRV("_minecraft._tcp.and", 1, 1, 39781, "vsc.pool.net.eu.org."),
+
+        // Eizel
+        A("eizel", "121.218.234.95"),
+        SRV("_minecraft._tcp.dorm", 1, 1, 9246, "eizel.pool.net.eu.org."),
+
 		// Email
 		TXT("_dmarc", "\"v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:nathan.kulzer@protonmail.com\""),
 		TXT("@", "\"v=spf1 -all\""),
 		TXT("*._domainkey", "\"v=DKIM1; p=\""),
 
+        // Jitpack verification
+		TXT("git", "https://github.com/PoolloverNathan"),
 		// Discord verification
-		TXT("@", "\"tcu5s0chyl\""),
-		TXT("_discord", '"dh=37a48a6b09bba850e08522dac7152c5a252faea9"'),
+		TXT("@", "tcu5s0chyl"),
+		TXT("_discord", "dh=37a48a6b09bba850e08522dac7152c5a252faea9"),
 	)
 })
