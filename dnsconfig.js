8b25af5e93d7d1267ed670fa7a9a5f928e010bb7
poolcritter <nathan.kulzer@protonmail.com>
2026-01-13 12:53:05 -0500

    changes

--- dnsconfig.js
+++ dnsconfig.js
@@ -24,22 +24,27 @@ MY_IP$.then(function(ip) {
         CNAME("phone", "h.pool.net.eu.org.", CF_PROXY_ON),
         CNAME("vsc", "h.pool.net.eu.org.", TTL(60)),
         //{{{2 Anti-email
-        TXT("_dmarc", "\"v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:nathan.kulzer@protonmail.com\""),
-        TXT("@", "\"v=spf1 -all\""),
-        TXT("*._domainkey", "\"v=DKIM1; p=\""),
+        //TXT("_dmarc", "\"v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:nathan.kulzer@protonmail.com\""),
+        //TXT("@", "\"v=spf1 -all\""),
+        //TXT("*._domainkey", "\"v=DKIM1; p=\""),
         //{{{2 Verification
         TXT("git", "https://github.com/PoolloverNathan"),
         TXT("@", "tcu5s0chyl"),
+        TXT("@", "abuseipdb-verification=y3TbgCsT"),
         TXT("_discord", "dh=37a48a6b09bba850e08522dac7152c5a252faea9"),
+        TXT("_discord", "dh=36757e6dc48db5fff8578eee766d16907a426e68"),
         //{{{2 & server
         SRV("_minecraft._tcp.and", 1, 1, 39781, "vsc.pool.net.eu.org."),
         //{{{1 Others
         //{{{2 Mavensburg
-        A("q", "50.92.163.215"),
+        A("q", "75.154.230.93"),
         CNAME("mavensburg", "q.pool.net.eu.org."),
         //{{{2 Eizel
         A("eizel", "121.218.234.95"),
         SRV("_minecraft._tcp.dorm", 1, 1, 9246, "eizel.pool.net.eu.org."),
+        //{{{2 Figura MSMP
+        A("figura-msmp-ip", "185.83.152.76"),
+        SRV("_minecraft._tcp.figura-msmp", 1, 1, 25566, "figura-msmp-ip.pool.net.eu.org."),
         //}}}1
     )
 })
